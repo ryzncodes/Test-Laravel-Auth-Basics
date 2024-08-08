6c96438b9807889c326bb5b9dfb39ce26a18e296
faizkasman97 <faizkasman@myra.com.my>
2024-08-08 18:21:22 +0800

    all test passed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,6 +16,17 @@ class ProfileController extends Controller
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
 
+        $user = auth()->user();
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        if ($request->filled('password')) {
+            $user->password = bcrypt($request->input('password'));
+        }
+
+        $user->save();
+
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
